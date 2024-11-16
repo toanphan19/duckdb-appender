ce49fd5b32683bd6b515c89c276630abed251655
Toan Phan <toan@Toans-MacBook-Pro.local>
2024-11-16 17:31:34 +0100

    test: add more tests for errors

--- test/test_errors.py
+++ test/test_errors.py
@@ -1,7 +1,12 @@
 import duckdb
 import pytest
 
-from pyappender.errors import AppendAfterCloseError, AppenderDoubleCloseError
+from pyappender.errors import (
+    AppendAfterCloseError,
+    AppenderDoubleCloseError,
+    TableNotFoundError,
+    ColumnCountError,
+)
 from pyappender.pyappender import Appender
 
 
@@ -9,6 +14,23 @@ TEST_DB = "test/test.duckdb"
 
 
 class TestErrors:
+
+    def test_table_not_found_error(self):
+        conn = duckdb.connect()
+        conn.execute("CREATE TABLE users (id INTEGER, name VARCHAR);")
+        conn.commit()
+
+        with pytest.raises(TableNotFoundError):
+            appender = Appender(conn, "main", "wrong_table_name")
+
+    def test_column_count_error(self):
+        conn = duckdb.connect()
+        conn.execute("CREATE TABLE users (id INTEGER, name VARCHAR);")
+        conn.commit()
+
+        appender = Appender(conn, "main", "users")
+        with pytest.raises(ColumnCountError):
+            appender.append_row([1, "John", "john@email.com"])
 
     def test_close_appender_twice(self):
         conn = duckdb.connect()
