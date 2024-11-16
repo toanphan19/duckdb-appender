ad8a2c95f15afaab90c37cdf5d6278bda82b5329
Toan <toanphan.dev@gmail.com>
2024-11-16 18:00:31 +0100

    docs: add example usage

--- README.md
+++ README.md
@@ -2,6 +2,23 @@
 
 A DuckDB appender for Python.
 
+## Usage
+
+```py
+conn = duckdb.connect()
+
+appender = Appender(conn, schema="main", table="users")
+appender.append_row([1, "John"])
+appender.append_row([2, "Doe"])
+appender.close()
+
+# Can also use it with context manager:
+with Appender(conn, schema="main", table="users") as appender:
+  appender.append_row([3, "foo"])
+```
+
+See more in the `/examles` folder.
+
 ## Local development
 
 This repo uses [uv](https://github.com/astral-sh/uv) to manage dependencies and
---
+++ examples/example.py
@@ -0,0 +1,31 @@
+import duckdb
+
+from pyappender import Appender
+
+if __name__ == "__main__":
+    # Set up the source DuckDB table
+    conn = duckdb.connect(":memory:")
+    conn.execute("CREATE TABLE users (id INTEGER, name VARCHAR);")
+    conn.commit()
+
+    # Use the appender
+    appender = Appender(conn, schema="main", table="users")
+    appender.append_row([1, "John"])
+
+    data_from_some_api = [
+        {"id": 11, "name": "User_11"},
+        {"id": 12, "name": "User_12"},
+        {"id": 13, "name": "User_13"},
+    ]
+    for d in data_from_some_api:
+        appender.append_row(d.values())
+
+    # Close the appender when you are done inserting.
+    appender.close()
+
+    with Appender(conn, schema="main", table="users") as appender:
+        appender.append_row([2, "Doe"])
+
+    print(conn.execute("SELECT * FROM users").fetchall())
+
+    conn.close()
